6f95b3de87ccc7ad2155075c52c6c3a97b8d90f9
Bhushan Wanjari <bhushanwanjari21z@gmail.com>
2022-05-22 19:00:01 +0530

    updated

--- apti_backend/apti_backend/handleDB.py
+++ apti_backend/apti_backend/handleDB.py
@@ -20,6 +20,18 @@ def get_all_questions():
 
 	return data
 
+def get_user_answers():
+    questions= db.collection('ques_bank').stream()
+    answers_temp={}
+    for q in questions:
+        doc=q.to_dict()
+        no=doc['no']
+        corr=doc['answer']
+        answers_temp[no]=corr
+        if(no%4==0 or no%5==0):
+            answers_temp[no]="wrong answer"
+      
+    return answers_temp
 
 def get_user_data(email):
 	users = db.collection("user").where(u'email', u'==', email).get()
