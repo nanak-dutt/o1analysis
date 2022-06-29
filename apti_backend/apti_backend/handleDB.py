ae4ec79b4015920bf08757f8c41e9533cd2b9d1c
kabirdutt0907 <NANAK@#dutt09>
2022-06-29 19:31:13 +0530

    Changes in Ranklist API

--- apti_backend/apti_backend/handleDB.py
+++ apti_backend/apti_backend/handleDB.py
@@ -184,7 +184,7 @@ def get_global_ranklist():
 		data.pop("level_wise_distribution")
 		data.pop("topic_wise_distribution")
 		data["rank"] = i
-		data["marks"] = data.pop("total_score")
+		data["total_score"] = data.pop("total_score")
 		i += 1
 		lst.append(data)
 
@@ -196,11 +196,14 @@ def get_college_ranklist(college):
 	query = users_ref.order_by(u'total_score', direction=firestore.Query.DESCENDING).stream()
 
 	lst = []
+	i = 1
 	for doc in query:
 		data = doc.to_dict()
 		data.pop("level_wise_distribution")
 		data.pop("topic_wise_distribution")
-		data.pop("college")
+		data["rank"] = i
+		i += 1
+		# data.pop("college")
 		lst.append(data)
 
 	i = 1
